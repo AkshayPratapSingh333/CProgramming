7c6b9dca49be7b157fce843e8e4918d7c5e0c44a
Akshay Pratap Singh <akshaypsingh333.devac@gmail.com>
2024-10-01 22:41:27 +0530

    Revision

--- 30_Array3D.c
+++ 30_Array3D.c
@@ -1,17 +1,56 @@
-#include<stdio.h>
-int main(){
-    int a[2][2][3] = { {{1,2,3},{4,5,6}},{{7,8,9},{10,11,12}} };
-    int i,j,k;
+// #include<stdio.h>
+// int main(){
+//     int a[2][2][3] = { {{1,2,3},{4,5,6}},{{7,8,9},{10,11,12}} };
+//     int i,j,k;
 
-    for(i=0;i<2;i++)
-    {
-        for(j=0;j<2;j++)
-        {
-            for(k=0;k<3;k++)
-            {
-                printf("%d\n",a[i][j][k]);
-            }
+//     for(i=0;i<2;i++)
+//     {
+//         for(j=0;j<2;j++)
+//         {
+//             for(k=0;k<3;k++)
+//             {
+//                 printf("%d\n",a[i][j][k]);
+//             }
+//         }
+//     }
+//     return 0;       
+// }
+#include <stdio.h>
+
+#define ROWS 3 // Number of rows in the 2D array
+#define COLS 4 // Number of columns in the 2D array
+
+// Function to initialize a 2D array with values
+void initializeArray(int arr[ROWS][COLS]) {
+    printf("Initializing 2D array:\n");
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("Enter value for arr[%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
         }
     }
-    return 0;       
+}
+
+// Function to display the 2D array
+void displayArray(int arr[ROWS][COLS]) {
+    printf("\nDisplaying 2D array:\n");
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int array[ROWS][COLS];
+
+    // Initialize the 2D array
+    initializeArray(array);
+
+    // Display the 2D array
+    displayArray(array);
+
+
+    return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,44 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
+# include <stdio.h>
+int main() {
+int marks[3];
+printf("physics : ");
+scanf("%d", &marks[0]);
+printf("chem : ");
+scanf("%d", &marks[1]);
+printf("math : ");
+scanf("%d", &marks[2]);
+printf("physics = %d, ", marks[0]); //physics
+printf("chem = %d, ", marks[1]); //chem
+printf("math = %d \n", marks[2]); //math
+       
+       return 0;
 
-struct Node
-{
-int data;
-struct Node *next;
-};
-
-void traverseList(struct Node *head) {
-    struct Node *temp = head;  // temp initially points to head (the first node)
-
-    printf("Linked list elements: ");
-    while (temp != NULL) {     // Loop through the list until temp becomes NULL
-        printf("%d -> ", temp->data);  // Print the data of the current node
-        temp = temp->next;     // Move temp to the next node
-    }
-    printf("NULL\n");  // Indicate the end of the list
-}
-
-
-int main(){
-    // creation of pointers
-    struct Node *head;
-    struct Node *first;
-    struct Node *second;
-
-    // Memory allocation for nodes in heap
-
-    head = (struct Node*)malloc(sizeof(struct Node));
-    first = (struct Node*)malloc(sizeof(struct Node));
-    second = (struct Node*)malloc(sizeof(struct Node));
-
-    // Linking head and first node 
-    head->data=2;
-    head->next = first;
-    // Linking first and second node 
-    first->data=4;
-    first->next = second;
-    //Terminating list at second node 
-    second->data=6;
-    second->next = NULL;
-    return 0;
 }
